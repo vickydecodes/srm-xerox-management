51b0eef6f297c6be5bb5cff7d35b73b22f29cf80
vasanthan-hub <vasanthanr2004@gmail.com>
2026-08-01 14:27:19 +0530

    feat: implement branch module

---
+++ backend/src/typings/branch.types.ts
@@ -0,0 +1,5 @@
+import { branchSchema } from '@modules/branch/branch.validator.ts';
+import z from 'zod';
+
+export type CreateBranchPayload = z.infer<typeof branchSchema>;
+export type UpdateBranchPayload = Partial<CreateBranchPayload>;
