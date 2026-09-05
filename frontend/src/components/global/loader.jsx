c6fc4eed13cbc3e3cc1f874083614c8d7830e253
rkgit-1530 <rajkaranprem@outlook.com>
2026-09-05 14:01:42 +0530

    feat: added srm logo in the layout and loader

--- frontend/src/components/global/loader.jsx
+++ frontend/src/components/global/loader.jsx
@@ -2,7 +2,7 @@ export default function AppLoader({ size = 150 }) {
   return (
     <div className="flex items-center justify-center h-screen w-screen">
       <img
-        src="/logo.png"
+        src="/logo1.png"
         alt="Loading"
         style={{ width: size, height: 180 }}
         className="animate-logo-intro"
